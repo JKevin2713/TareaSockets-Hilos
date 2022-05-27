612d84044c06fdafbff2be720176a0a32a0a1219
Kevin Jiménez <kevin201560@estudiantec.cr>
2022-05-27 15:17:51 -0600

    Tarea Sockets e Hilos

---
+++ dotGame/src/Client/ServerClient.java
@@ -0,0 +1,45 @@
+package Client;
+
+import java.net.*;
+import java.io.*;
+import Common.*;
+
+public class ServerClient implements Runnable{
+    ServerSocket server;
+    Socket client;
+    ObjectInputStream input;
+    Dot dot;
+
+    public ServerClient(Dot d){
+        dot = d;
+        try {
+            server = new ServerSocket(4445);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        
+    }
+
+    public void run(){
+        try {
+            while(true){
+                client = server.accept();
+                input = new ObjectInputStream(client.getInputStream());
+                Dot dotTemp = (Dot)input.readObject();
+                dot.currentPosition[0] = dotTemp.currentPosition[0];
+                dot.currentPosition[1] = dotTemp.currentPosition[1];
+                dot.lastPosition[0] = dotTemp.lastPosition[0];
+                dot.lastPosition[1] = dotTemp.lastPosition[1];
+                System.out.println("Dont x: "+ dot.currentPosition[0]+" Dot y: "+dot.currentPosition[1]);
+                //dot.currentPosition[X] = dot.target.coords[X];
+                //dot.currentPosition[Y] = dot.target.coords[Y];
+                input.close();
+                client.close();
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+    }
+    
+}
